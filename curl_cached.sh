5cdc46ab6b70138e2595d055b93847fe84da54dc
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-03-16 11:18:39 +0200

    optimize curl_cached

--- curl_cached.sh
+++ curl_cached.sh
@@ -1,19 +1,27 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p dash cacert curl flock
+#! nix-shell --pure --keep CURL_CACHED_MINUTES --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p dash cacert curl flock
 set -eu
 
 url="$1"
-minutes="${2:-1}"
+
+minutes="${CURL_CACHED_MINUTES:-1}"
 
 outputfile="${XDG_RUNTIME_DIR:-/tmp}/$(basename "$PWD")/$(echo -n "$url" | tr -c '[:alnum:]' '_')"
 
-test -e "$(dirname "$outputfile")" || mkdir -p "$(dirname "$outputfile")"
+for i in $(find "$outputfile" -mmin -$minutes); do
+    if [ -s "$outputfile" ]; then
+        cat "$outputfile"
+        exit 0
+    fi
+done
 
 (
     flock 8
 
+    test -e "$(dirname "$outputfile")" || mkdir -p "$(dirname "$outputfile")"
+
     fetch() {
-        curl -4 --no-progress-meter -L "$url" -o "$outputfile"
+        curl --no-progress-meter -L -o "$outputfile" "$url" $*
     }
 
     if [ ! -f "$outputfile" ] || [ ! -s "$outputfile" ]; then
