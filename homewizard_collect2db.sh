fd514aa42042a8c6601a2ab63b1b3c2b4a18a224
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-05-31 16:08:31 +0300

    ...

--- homewizard_collect2db.sh
+++ homewizard_collect2db.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep LD_LIBRARY_PATH -i dash -I channel:nixos-23.11-small -p nix dash sqlite coreutils gnused curl cacert flock bc jq
+#! nix-shell --pure --keep LD_LIBRARY_PATH --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p nix dash sqlite coreutils gnused curl cacert flock bc jq
 set -eu
 
 export LC_ALL=C # "fix" Nix Perl locale warnings
