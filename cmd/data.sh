8726674620937b2fc52269c5db4f1aee8eb953f8
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2025-01-21 20:35:45 +0200

    update nix

--- cmd/data.sh
+++ cmd/data.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep LD_LIBRARY_PATH --keep XDG_RUNTIME_DIR --keep CURL_CACHED_MINUTES -i dash -I channel:nixos-23.11-small -p nix dash cacert curl gnused flock jq
+#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep LD_LIBRARY_PATH --keep XDG_RUNTIME_DIR --keep CURL_CACHED_MINUTES -i dash -I channel:nixos-24.11-small -p nix dash cacert curl gnused flock jq
 set -eu
 
 key="${1:-}"
