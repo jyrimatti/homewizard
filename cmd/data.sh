307dcf1275e1e9bd19352e231c7e6b6cc36131e0
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2025-02-11 19:48:59 +0200

    add bkt and cleanup

--- cmd/data.sh
+++ cmd/data.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep LD_LIBRARY_PATH --keep XDG_RUNTIME_DIR --keep CURL_CACHED_MINUTES -i dash -I channel:nixos-24.11-small -p nix dash cacert curl gnused flock jq
+#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep LD_LIBRARY_PATH --keep BKT_SCOPE --keep BKT_CACHE_DIR
+#! nix-shell -i dash -I channel:nixos-24.11-small -p nix dash cacert curl jq bkt
 set -eu
 
 key="${1:-}"
@@ -9,6 +10,4 @@ else
     key=".$key"
 fi
 
-. ./homewizard_env.sh
-
-dash ./curl_cached.sh "http://$HOMEWIZARD_HOST/api/v1/data" | jq -r "$key"
+dash ./homewizard_get.sh | jq -r "$key"
---
+++ homewizard_get.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep LD_LIBRARY_PATH --keep BKT_SCOPE --keep BKT_CACHE_DIR
+#! nix-shell -i dash -I channel:nixos-24.11-small -p nix dash cacert curl bkt
+set -eu
+
+. ./homewizard_env.sh
+
+bkt --discard-failures --ttl 6s --stale 5s -- curl --no-progress-meter -L "http://$HOMEWIZARD_HOST/api/v1/data"
